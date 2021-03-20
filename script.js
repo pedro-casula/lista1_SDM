150543511134bb51d56761d5f554784862371ed3
Pedro Casula <pedro.casula@hotmail.com>
2021-03-20 18:17:19 -0300

    Iniciando a lista

---
+++ script.js
@@ -0,0 +1,61 @@
+// alert, prompt, confirm
+
+// alert ("Hello Word!");
+// prompt ("Digite seu nome: ");
+// confirm ("Você torce para qual time? ");
+
+/*
+function verificarAposentadoria (usuario) {
+
+    if (usuario.idade >= 50 ) {
+        return false;
+   }
+    else {
+        return true;
+    }
+
+}
+
+let usuario = {
+    nome: "Godofredo",
+    idade:"28",
+    eJogador: true 
+
+};
+
+let usuario2 = {
+    nome: "João ",
+    idade: "52",
+    ejogador: false ,
+    paisesQueMorou:[
+        'Espanha',
+        'Brasil',
+        'Argentina',
+        'BH',
+        
+    ]
+      
+    } 
+
+let usuarios = [usuario, usuario2];
+
+let ap1 = verificarAposentadoria(usuarios);
+
+console.log(usuarios);
+
+
+if (verificarAposentadoria(usuarios)){
+    console.log("Voce pode aposentar! ")
+
+}
+else{
+    console.log("Não pode aposentar! ")
+}
+
+*/
+
+// iniciando A lista de Exercícios 
+
+
+
+   
